c8a1bbc58918b7080bf25d761819fb464a9d1d5c
l-shahbil <laith.715.asad@gmail.com>
2025-01-22 17:07:48 +0300

    Tailwind Config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF3636',
+        secondry:{
+          1:'E2E2D5',
+          2:"#888883"
+        }
+      } 
+    },
+  },
+  variants: {},
+  plugins: [],
+}
